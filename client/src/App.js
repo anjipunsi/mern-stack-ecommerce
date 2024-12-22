48c54ba8f371b7846fa0b38e37ebb97b352232de
Anjali punsi <anjalipunsi2@gmail.com>
2024-12-22 15:48:04 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      {/* Other components like Routes, Main Content */}
+    </div>
+  );
+};
+
+export default App;
